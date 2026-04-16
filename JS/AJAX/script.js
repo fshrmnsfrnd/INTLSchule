e5aaeba8cd8cdfbac40d27e5882c3bbfd17df8bb
fshrmnsfrnd <fshrmnsfrnd@web.de>
2026-04-16 08:30:26 +0200

    ajax uebung 2

--- JS/AJAX/script.js
+++ JS/AJAX/script.js
@@ -1,37 +1,42 @@
 const inputField = document.getElementById("input")
 let outputField = document.getElementById("output")
 let detailsField = document.getElementById("details")
-const searchBtn = document.getElementById("search")
+//const searchBtn = document.getElementById("search")
 const request = new XMLHttpRequest()
 const url = "http://localhost/adressen.php"
-const detailRequest = new XMLHttpRequest()
 
-request.onload = function () {
-    try{
-        printOutput(JSON.parse(this.responseText))
-    }catch(e){
-        console.log(e)
-    }
-}
-
-detailRequest.onload = function () {
-    try{
-        printDetails(JSON.parse(this.responseText))
-    }catch(e){
-        console.log(e)
+function fetchTextSearch(text){
+    request.open("get", url+"?suche="+text)
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    request.send(null)
+    request.onload = function () {
+        try{
+            printOutput(JSON.parse(this.responseText))
+        }catch(e){
+            console.log(e)
+        }
     }
 }
 
 function fetchDetails(id){
-    detailRequest.open("get", url+"?details="+id)
-    detailRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    detailRequest.send(null)
+    request.open("get", url+"?details="+id)
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    request.send(null)
+    request.onload = function () {
+        try{
+            printDetails(JSON.parse(this.responseText))
+        }catch(e){
+            console.log(e)
+        }
+    }
 }
 
 inputField.addEventListener("change", ()=>{
-    request.open("get", url+"?suche="+inputField.value)
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    request.send(null)
+    if(parseInt(inputField.value).toString() === NaN.toString()){
+        fetchTextSearch(inputField.value)
+    }else{
+        fetchDetails(inputField.value)
+    }
 })
 
 function printOutput(json){
@@ -46,6 +51,13 @@ function printOutput(json){
     });
 }
 
-function printDetails(json){
-    detailsField.innerText = JSON.stringify(json)
+function printDetails(data){
+    console.log(data)
+    document.getElementById("nname").value = data.name
+    document.getElementById("vname").value = data.vorname
+    document.getElementById("strasse").value = data.strasse
+    document.getElementById("plz").value = data.plz
+    document.getElementById("ort").value = data.ort
+    document.getElementById("id").value = data.id
+    document.getElementById("email").value = data.email
 }
